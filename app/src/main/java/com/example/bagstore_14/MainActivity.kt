c567c7cf039148b23e1274863eff97a0eb51ef20
ParvizSharifi96 <parviz.sharifi1996@gmail.com>
2023-03-14 13:40:10 +0330

    Add navigation

--- app/src/main/java/com/example/bagstore_14/MainActivity.kt
+++ app/src/main/java/com/example/bagstore_14/MainActivity.kt
@@ -6,6 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.bagstore_14.ui.theme.MainAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,21 +22,113 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun BagStoreUi(){
+fun BagStoreUi() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "mainScreen"
+    ) {
+        composable("mainScreen") {
+            MainScreen()
+        }
 
+        composable(
+            route = "productScreen/{productId}",
+        arguments = listOf(navArgument("productId"){
+            type= NavType.IntType
+        })
+        ) {
+            ProductScreen(it.arguments!!.getInt("productId",-1))
+        }
+
+        composable(
+            route = "categoryScreen",
+        arguments = listOf(navArgument("categoryName"){
+            type = NavType.StringType
+        })
+        ) {
+            CategoryScreen(it.arguments!!.getString("categoryName","null"))
+        }
+
+        composable("profileScreen") {
+            ProfileScreen()
+        }
+
+        composable("cartScreen") {
+            CartScreen()
+        }
+
+        composable("singUpScreen") {
+            SingUpScreen()
+        }
+
+        composable("singInScreen") {
+            SingInScreen()
+        }
+
+        composable("introScreen") {
+            IntroScreen()
+        }
+
+        composable("noInternetScreen") {
+            NoInternetScreen()
+        }
+
+
+    }
 
 
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NoInternetScreen() {
+
+}
+
+@Composable
+fun IntroScreen() {
+
+}
+
+@Composable
+fun SingInScreen() {
+
+}
+
+@Composable
+fun SingUpScreen() {
+
+}
+
+@Composable
+fun CartScreen() {
+
+}
+
+@Composable
+fun ProfileScreen() {
+
+}
+
+@Composable
+fun CategoryScreen(categoryName : String) {
+
+}
+
+@Composable
+fun ProductScreen(productId:Int) {
+
+}
+
+@Composable
+fun MainScreen() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MainAppTheme {
-        Greeting("Android")
+        BagStoreUi()
     }
 }
