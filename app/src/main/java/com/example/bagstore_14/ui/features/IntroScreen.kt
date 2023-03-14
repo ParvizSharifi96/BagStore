7349fa30108050f0f27bd72cef2985fadf615ccf
ParvizSharifi96 <parviz.sharifi1996@gmail.com>
2023-03-14 15:33:41 +0330

    Implementation first page

--- app/src/main/java/com/example/bagstore_14/ui/features/IntroScreen.kt
+++ app/src/main/java/com/example/bagstore_14/ui/features/IntroScreen.kt
@@ -1,24 +1,31 @@
 package com.example.bagstore_14.ui.features
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MovableContent
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bagstore_14.ui.BagStoreUi
+import com.example.bagstore_14.R
 import com.example.bagstore_14.ui.theme.BackgroundMain
+import com.example.bagstore_14.ui.theme.Blue
 import com.example.bagstore_14.ui.theme.MainAppTheme
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun IntroScreenPreview() {
     MainAppTheme {
 
         Surface(
-            color = BackgroundMain ,
+            color = BackgroundMain,
             modifier = Modifier.fillMaxSize()
         ) {
             IntroScreen()
@@ -30,5 +37,43 @@ fun DefaultPreview() {
 
 @Composable
 fun IntroScreen() {
+
+    Image(
+        modifier = Modifier.fillMaxSize(),
+        painter = painterResource(R.drawable.img_intro), contentDescription = null,
+        contentScale = ContentScale.Crop
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.78f),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+
+        Button(
+            modifier = Modifier.fillMaxWidth(0.7f),
+            onClick = {  }
+        ) {
+            Text(
+                text = "Sign Up"
+            )
+        }
+
+
+        Button(
+            modifier = Modifier.fillMaxWidth(0.7f),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            onClick = { }
+        ) {
+            Text(
+                text = "Sign In" ,
+                color = Blue
+            )
+        }
+
+    }
+
 
 }
