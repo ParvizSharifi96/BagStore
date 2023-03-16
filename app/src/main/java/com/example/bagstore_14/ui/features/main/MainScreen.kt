c50f1bc88dfe65bb09bcd1ead436263a78e4b061
ParvizSharifi96 <parviz.sharifi1996@gmail.com>
2023-03-16 12:57:56 +0330

    Category MainScreen

--- app/src/main/java/com/example/bagstore_14/ui/features/main/MainScreen.kt
+++ app/src/main/java/com/example/bagstore_14/ui/features/main/MainScreen.kt
@@ -1,9 +1,12 @@
 package com.example.bagstore_14.ui.features.main
 
-import android.app.Notification.BigPictureStyle
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
@@ -12,19 +15,24 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.bagstore_14.ui.CategoryScreen
+import com.example.bagstore_14.R
 import com.example.bagstore_14.ui.theme.BackgroundMain
+import com.example.bagstore_14.ui.theme.CardViewBackground
 import com.example.bagstore_14.ui.theme.MainAppTheme
-import com.example.bagstore_14.util.MyScreens
+import com.example.bagstore_14.ui.theme.Shapes
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
 
 @Preview(showBackground = true)
 @Composable
-fun MainScreePreview(){
+fun MainScreePreview() {
 
     MainAppTheme {
         Surface(
@@ -38,9 +46,9 @@ fun MainScreePreview(){
 
 
 @Composable
-fun MainScreen(){
+fun MainScreen() {
     val uiController = rememberSystemUiController()
-    SideEffect {uiController.setStatusBarColor(Color.White)}
+    SideEffect { uiController.setStatusBarColor(Color.White) }
 
     Column(
         modifier = Modifier
@@ -49,7 +57,7 @@ fun MainScreen(){
             .padding(bottom = 16.dp)
 
     ) {
-         TopToolbar()
+        TopToolbar()
 
         CategoryBar()
 
@@ -70,7 +78,7 @@ fun TopToolbar() {
     TopAppBar(
         elevation = 0.dp,
         backgroundColor = Color.White,
-        title = { Text(text = "Duni Bazaar") },
+        title = { Text(text = "BagStore-14") },
         actions = {
 
             IconButton(onClick = {}) {
@@ -90,6 +98,47 @@ fun TopToolbar() {
 
 @Composable
 fun CategoryBar() {
+
+    LazyRow(
+        modifier = Modifier.padding(top = 16.dp),
+        contentPadding = PaddingValues(end = 16.dp),
+    ) {
+        items(10) {
+            CategoryItem()
+        }
+    }
+
+
+}
+
+@Composable
+fun CategoryItem() {
+
+    Column(
+        modifier = Modifier
+            .padding(start = 16.dp)
+            .clickable { },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Surface(
+            shape = Shapes.medium,
+            color = CardViewBackground
+        ) {
+
+            Image(
+                modifier = Modifier.padding(16.dp),
+                painter = painterResource(id = R.drawable.ic_icon_app),
+                contentDescription = null
+            )
+
+        }
+        Text(
+            text = "Hotels",
+            modifier = Modifier.padding(top = 4.dp),
+            style = TextStyle(color = Color.Gray)
+        )
+    }
 
 }
 //------------------------------------------------------------------------------
