0ce2e44480ec4e62b35bb67392802b1eeb398d07
ParvizSharifi96 <parviz.sharifi1996@gmail.com>
2023-03-16 17:16:22 +0330

    productRepository(checkInternet)

--- app/src/main/java/com/example/bagstore_14/model/repository/product/ProductRepository.kt
+++ app/src/main/java/com/example/bagstore_14/model/repository/product/ProductRepository.kt
@@ -5,9 +5,9 @@ import com.example.bagstore_14.model.data.Product
 
 interface ProductRepository {
 
-  suspend  fun getAllProducts() : List<Product>
+  suspend  fun getAllProducts(isInternetConnected : Boolean) : List<Product>
 
-    suspend fun getAllAds() : List<Ads>
+    suspend fun getAllAds(isInternetConnected : Boolean) : List<Ads>
 
 
 
--- app/src/main/java/com/example/bagstore_14/model/repository/product/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/bagstore_14/model/repository/product/ProductRepositoryImpl.kt
@@ -5,15 +5,47 @@ import com.example.bagstore_14.model.data.Product
 import com.example.bagstore_14.model.db.ProductDao
 import com.example.bagstore_14.model.net.ApiService
 
-//class ProductRepositoryImpl(
-//    private val apiService: ApiService,
-//    private val productDao: ProductDao
-//    ) : ProductRepository {
-//    override suspend fun getAllProducts(): List<Product> {
-//
-//    }
+class ProductRepositoryImpl(
+    private val apiService: ApiService,
+    private val productDao: ProductDao
+    ) : ProductRepository {
+    override suspend fun getAllProducts(isInternetConnected : Boolean): List<Product> {
 
-//    override suspend fun getAllAds(): List<Ads> {
-//
-//    }
-//}
+
+        if(isInternetConnected) {
+
+            // get data from net
+            val dataFromServer = apiService.getAllProducts()
+            if(dataFromServer.success) {
+                productDao.insertOrUpdate(dataFromServer.products)
+                return dataFromServer.products
+            }
+
+        } else {
+
+            // get data from local
+            return productDao.getAll()
+
+        }
+
+        return listOf()
+    }
+
+    override suspend fun getAllAds(isInternetConnected : Boolean): List<Ads> {
+        if(isInternetConnected) {
+
+            // get ads
+
+            val dataFromServer = apiService.getAllAds()
+            if(dataFromServer.success) {
+                return dataFromServer.ads
+            }
+
+        }
+
+
+        return listOf()
+    }
+
+
+}
